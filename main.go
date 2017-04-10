5e854240680818a87e1eb256f1766e5a3cc9b76d
forteleaf.aurora <forteleaf@gmail.com>
2017-04-10 17:53:32 +0900

    append slice

--- main.go
+++ main.go
@@ -19,26 +19,26 @@ func sum(a, b, c int) {
 	if result5 > 81 {
 		result5 = result5 - 80
 	}
-	func() {
-		fmt.Println("[", a, b, c, "]")
-		fmt.Println("천격", result1)
-		fmt.Println("인격", result2)
-		fmt.Println("지격", result3)
-		fmt.Println("외격", result4)
-		fmt.Println("총격", result5)
-	}()
+	// func() {
+	// 	fmt.Println("[", a, b, c, "]")
+	// 	fmt.Println("천격", result1)
+	// 	fmt.Println("인격", result2)
+	// 	fmt.Println("지격", result3)
+	// 	fmt.Println("외격", result4)
+	// 	fmt.Println("총격", result5)
+	// }()
 
 	if BadorGood(result1) {
 		// fmt.Println("천격통과")
 		if BadorGood(result5) {
-			fmt.Println("총격통과")
-			fmt.Println("[", a, b, c, "]")
+			// fmt.Println("총격통과")
 			if BadorGood(result3) {
-				fmt.Println("지격통과")
+				// fmt.Println("지격통과")
 				if BadorGood(result4) {
-					fmt.Println("외격통과")
+					// fmt.Println("외격통과")
 					if BadorGood(result2) {
-						fmt.Println("중격통과")
+						fmt.Println("[", a, b, c, "]")
+						// fmt.Println("중격통과")
 					}
 				}
 			}
@@ -47,26 +47,29 @@ func sum(a, b, c int) {
 
 }
 
+// 길흉 정리
 func BadorGood(num int) bool {
+	//http://www.finename.co.kr/gnu/bbs/board.php?bo_table=m21&wr_id=28 자료를 통해서 정리
 
-	// goodArr := []int{}
-	var numPerfect = []int{13, 16, 21, 23, 31, 41}
-	// var numVerygood = []int{1, 3, 5, 6, 11, 15, 18, 24, 32, 35, 37, 39}
-	// var numGood = []int{7, 8, 17, 25, 29, 33, 38, 45, 47, 48, 52, 57, 58, 61, 63, 65, 67, 68, 71, 73, 75, 77, 81}
-	// var numBad = []int{2, 4, 12, 14, 27, 28, 30, 53, 59, 60, 62, 69, 72}
-	// var numHell = []int{9, 10, 19, 20, 22, 26, 34, 36, 40, 42, 43, 44, 46, 50, 54, 56, 64, 66, 70, 74, 78, 80}
+	var ok bool = false
+	var arr = []int{}
+	var numPerfect = []int{13, 16, 21, 23, 31, 41}                      // 최상운수
+	var numVerygood = []int{1, 3, 5, 6, 11, 15, 18, 24, 32, 35, 37, 39} // 상운수
+	// var numGood = []int{7, 8, 17, 25, 29, 33, 38, 45, 47, 48, 52, 57, 58, 61, 63, 65, 67, 68, 71, 73, 75, 77, 81} // 양운수
+	// var numBad = []int{2, 4, 12, 14, 27, 28, 30, 53, 59, 60, 62, 69, 72} // 흉운수
+	// var numHell = []int{9, 10, 19, 20, 22, 26, 34, 36, 40, 42, 43, 44, 46, 50, 54, 56, 64, 66, 70, 74, 78, 80} // 흉흉운수
+	arr = append(numPerfect, numVerygood...)
+	// arr = numPerfect
 
-	for _, v := range numPerfect {
+	for _, v := range arr {
 		if num == v {
-			// fmt.Println("insert num is", num)
-			// goodArr = append(goodArr, num)
-			return true
+			ok = true
 		}
 	}
-	return false
+	return ok
 }
 
-// 첫획을 넣으면 나머지 좋은 획수 구하기
+// 입력받아서 체크하기
 func InputNum() {
 	var n1, n2, n3 int
 	fmt.Print("Enter number:")
@@ -75,12 +78,16 @@ func InputNum() {
 	fmt.Scanf("%d", &n3)
 	sum(n1, n2, n3)
 }
+
+// 첫획을 넣으면 나머지 좋은 획수 구하기
 func main() {
-	var wordA = 12 // 황
+	// var wordA = 12 // 황
 	// reader := bufio.NewReader(os.Stdin)
 	for i := StrStroke; i <= EndStroke; i++ {
 		for j := StrStroke; j <= EndStroke; j++ {
-			sum(wordA, i, j)
+			for k := StrStroke; k <= EndStroke; k++ {
+				sum(i, j, k)
+			}
 		}
 	}
 
